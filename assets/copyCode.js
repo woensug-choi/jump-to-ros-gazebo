cc543cc0acb679e5657ea3c4f9d73dfeebc63c94
Woen-Sug Choi <woensug.choi@gmail.com>
2023-02-04 11:58:05 +0900

    copy code trial 3

--- assets/copyCode.js
+++ assets/copyCode.js
@@ -1,25 +1,23 @@
-// This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+var codeBlocks = document.querySelectorAll('pre.highlight');
 
-copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
 
-  copyCodeButton.addEventListener('click', () => {
-    // Copy the code to the user's clipboard
+  codeBlock.append(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Copied!';
+    copyButton.innerText = 'Copied';
+    var fourSeconds = 4000;
 
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
-
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
-    }, 2000);
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+    }, fourSeconds);
   });
 });
